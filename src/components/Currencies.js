4e801c07f2ad22153d2bde80f55baccf662b2355
Andrii Lishchyna <andrej044@i.ua>
2023-11-22 15:39:36 -0500

    created Currencies component

---
+++ src/components/Currencies.js
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react';
+// import { AppContext } from '../context/AppContext';
+const Currencies = () => {
+
+    return (
+        <div className='alert alert-secondary'>
+            <span>Currencies</span>
+        </div>
+    );
+};
+export default Currencies;
